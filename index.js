e6bc3985b21fbaa73da025accd1e838001f996a2
Mohammad Sufian <91954762+Suf42@users.noreply.github.com>
2024-08-22 17:56:42 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function redirectToLinkedIn(){ window.location = "https://www.linkedin.com/in/sufian-mohammad-798830230"; }
+
+function redirectToGitHub(){ window.location = "https://github.com/Suf42"; }
+
+document.getElementById("button-LinkedIn").addEventListener("click", redirectToLinkedIn);
+
+document.getElementById("button-GitHub").addEventListener("click", redirectToGitHub);
