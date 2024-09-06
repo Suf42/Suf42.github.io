5a9fc6d45efeff62a0021d7427cbc143efb103a3
Mohammad Sufian <91954762+Suf42@users.noreply.github.com>
2024-09-07 00:07:37 +0800

    Update design of experience section

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function redirectToLinkedIn(){ window.location = "https://www.linkedin.com/in/sufian-mohammad-798830230"; }
+// function redirectToLinkedIn(){ window.location = "https://www.linkedin.com/in/sufian-mohammad-798830230"; }
 
-function redirectToGitHub(){ window.location = "https://github.com/Suf42"; }
+// function redirectToGitHub(){ window.location = "https://github.com/Suf42"; }
+
+function redirectToLinkedIn(){ window.open("https://www.linkedin.com/in/sufian-mohammad-798830230", "_blank"); }
+
+function redirectToGitHub(){ window.open("https://github.com/Suf42", "_blank"); }
 
 document.getElementById("button-LinkedIn").addEventListener("click", redirectToLinkedIn);
 
